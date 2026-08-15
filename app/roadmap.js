d2336bd19b88ae1f497a37bb24679b4bf7efab44
Darin Spence <darinspence@gmail.com>
2026-08-15 16:53:31 -0600

    Refresh first 20 expenses

--- app/roadmap.js
+++ app/roadmap.js
@@ -1,4 +1,4 @@
-const roadmapSource = "https://raw.githubusercontent.com/DarinSpence/lsl/4e7cc2e/docs/ROADMAP.md";
+const roadmapSource = "https://raw.githubusercontent.com/DarinSpence/lsl/ecd6b35/docs/ROADMAP.md";
 const roadmapContent = document.querySelector("#roadmap-content");
 
 function textNode(value) {
